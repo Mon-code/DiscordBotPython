2bc8dc181cec34807388ae41b3f56e4e118d4ac9
demonn <70189601+Mon-code@users.noreply.github.com>
2021-05-02 20:11:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from discord.ext import commands
 import os
 
 client = commands.Bot(command_prefix= "!")
-path = r"\"
+path = r"\your directory here"
 dir = os.listdir(path)
 
 for file in dir:
     if file.endswith('.py'):
         client.load_extension(f'cogs.{file[:-3]}')
 
-client.run('NzY5NTMxNDk5OTQ2NDQyODEy.X5QYDg._ApGLNBICeIxlo32a1JVtxf0PWU')
+client.run('your bot token')
